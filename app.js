2ba9e51f917256c3c0e9e79bd8c03c8059774e17
mohammad <mofahimi1384@gmail.com>
2025-04-21 15:34:38 +0330

    data added to the DB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require("helmet");
 const config = require("config");
 const userRouter = require("./router/user");
+const mongoose = require("mongoose");
 
 
 const app = express();
@@ -10,6 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/vidly")
+  .then(() => console.log("connected to mongodb..."))
+  .catch((err) => console.error("could not connect to mongodb..."));
 
 
 app.use("/api/users", userRouter )
--- router/user.js
+++ router/user.js
@@ -2,20 +2,25 @@ const express = require('express');
 const users = require("../users");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
+const User = require("../models/user");
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const users = await User.find();
+  if (!users) {
+    return res.status(404).send("there is no user");
+  }
   res.json(users);
 });
 
 
-router.get("/:id", (req, res) => {
-  const finduser = users.find(user => { return user.id === parseInt(req.params.id); });
-  if (!finduser) {
+router.get("/:id", async (req, res) => {
+  const users = await User.findById(req.params.id);
+  if (!users) {
     res.status(404).send("there is no user whit this id");
   }
   res.json({
-    data: finduser,
+    data: users,
     messege: "ok"
   });
 });
@@ -24,7 +29,7 @@ router.post("/", [
   body("email", "email is required").isEmail(),
   body("first_name", "first_name is required").notEmpty(),
   body("last_name", "last_name is required").notEmpty(),
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({
@@ -32,11 +37,16 @@ router.post("/", [
       message: "validation error"
     });
   }
-  users.push({
-    id: users.length + 7, ...req.body
-  });
+  let newUser = new User({
+    id: req.body.id,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    avatar: req.body.avatar
+  })
+  newUser = await newUser.save();
   res.json({
-    data: users,
+    data: newUser,
     message: "ok"
   })
 })
@@ -45,7 +55,7 @@ router.put("/:id", [
   body("email", "email is required").isEmail(),
   body("first_name", "first_name is required").notEmpty(),
   body("last_name", "last_name is required").notEmpty(),
-], (req, res) => {
+], async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({
@@ -53,24 +63,29 @@ router.put("/:id", [
       message: "validation error"
     });
   }
-  const index = users.findIndex( user => user.id === parseInt(req.params.id));
-  if (index === -1) {
+  const user = await User.findByIdAndUpdate(req.params.id, {
+    id: req.body.id,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    avatar: req.body.avatar
+  }, { new: true }
+  );
+  if (!user) {
     return res.status(404).send("User not found");
   }
-  users[index] = { ...users[index], ...req.body };
-
   res.json({
-    data: users,
+    data: user,
     message: "ok"
   })
 })
 
-router.delete("/:id", (req, res) => {
-  const index = users.findIndex( user => user.id === parseInt(req.params.id));
-  if (index === -1) {
+router.delete("/:id", async(req, res) => {
+  const user =await User.findByIdAndDelete(req.params.id);
+  const users = await User.find();
+  if (!user){
     return res.status(404).send("User not found");
   }
-  users.splice(index, 1);
   res.json({
     data: users,
     message: "ok"
